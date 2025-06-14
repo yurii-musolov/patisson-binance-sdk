86b019d76d028a6726dd9238bbf4d4a3465d2287
Yurii Musolov <yurii.musolov@gmail.com>
2025-06-14 17:52:47 +0200

    feat(error): add custom error

--- src/spot/error.rs
+++ src/spot/error.rs
@@ -236,3 +236,75 @@ pub enum ErrorCode {
     /// Client order ID is not correct for this order ID.
     ClientOrderIdInvalid = -2039,
 }
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    Msg(String),
+    Reqwest(reqwest::Error),
+    SerdeJson(serde_json::Error),
+    SerdeUrlEncoded(serde_urlencoded::ser::Error),
+    SerdePathToError(serde_path_to_error::Error<serde_json::Error>),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Io(error) => write!(f, "I/O error: {error}"),
+            Error::Msg(msg) => write!(f, "{msg}"),
+            Error::Reqwest(error) => write!(f, "reqwest error: {error}"),
+            Error::SerdeJson(error) => write!(f, "serde_json error: {error}"),
+            Error::SerdeUrlEncoded(error) => write!(f, "serde_urlencoded error: {error}"),
+            Error::SerdePathToError(error) => write!(
+                f,
+                "serde_path_to_error error: path: {}, msg: {}",
+                error.path(),
+                error.inner()
+            ),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
+impl From<String> for Error {
+    fn from(msg: String) -> Self {
+        Error::Msg(msg)
+    }
+}
+
+impl From<&str> for Error {
+    fn from(msg: &str) -> Self {
+        Error::Msg(msg.to_string())
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::Reqwest(err)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::SerdeJson(err)
+    }
+}
+
+impl From<serde_urlencoded::ser::Error> for Error {
+    fn from(err: serde_urlencoded::ser::Error) -> Self {
+        Error::SerdeUrlEncoded(err)
+    }
+}
+
+impl From<serde_path_to_error::Error<serde_json::Error>> for Error {
+    fn from(err: serde_path_to_error::Error<serde_json::Error>) -> Self {
+        Error::SerdePathToError(err)
+    }
+}
