2a897cf45b8a83fce9fd81d0ae9094a18e37bd93
Yurii Musolov <yurii.musolov@gmail.com>
2025-06-15 00:31:36 +0200

    feat(exmple): add example 'server-time'

---
+++ examples/server-time.rs
@@ -0,0 +1,23 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run --example server-time
+//! ```
+
+use tokio;
+
+use binance::spot::{BASE_URL_API, Client, ClientConfig};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cfg = ClientConfig {
+        base_url: BASE_URL_API.to_string(),
+        api_key: None,
+        api_secret: None,
+    };
+    let client = Client::new(cfg);
+    let response = client.get_server_time().await?;
+    println!("{response:#?}");
+
+    Ok(())
+}
