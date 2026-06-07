a56dc3173d6cd8d4a50de90fa79066cc322fa352
Yurii Musolov <yurii.musolov@gmail.com>
2026-06-07 17:03:53 +0200

    feat(examples/usdm): create examples

---
+++ examples/usdm-account-information.rs
@@ -0,0 +1,39 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run --example usdm-account-information
+//! ```
+
+use binance::{
+    SensitiveString,
+    derivatives::usds_margined_futures::{
+        BASE_URL_API,
+        http::{GetAccountInformationParams, PrivateClient, PrivateConfig},
+    },
+    timestamp,
+};
+use tokio;
+use tracing::{Level, info};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let api_key = std::env::var("API_KEY").expect("environment variable API_KEY is required");
+    let api_key = SensitiveString::from(api_key);
+    let api_secret =
+        std::env::var("API_SECRET").expect("environment variable API_SECRET is required");
+    let api_secret = SensitiveString::from(api_secret);
+    let cfg = PrivateConfig::new(BASE_URL_API, api_key, api_secret);
+    let client = PrivateClient::new(cfg);
+
+    let params = GetAccountInformationParams::new(timestamp());
+    let response = client.account_information(params).await?;
+    info!(?response, "response");
+
+    Ok(())
+}
---
+++ examples/usdm-server-time.rs
@@ -0,0 +1,35 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run --example usdm-server-time
+//! ```
+
+use std::time::Instant;
+
+use binance::derivatives::usds_margined_futures::{
+    BASE_URL_API,
+    http::{PublicClient, PublicConfig},
+};
+use tokio;
+use tracing::{Level, debug, info};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let cfg = PublicConfig::new(BASE_URL_API);
+    let client = PublicClient::new(cfg);
+
+    let start = Instant::now();
+    let response = client.get_server_time().await?;
+    let duration = start.elapsed();
+
+    info!(?response, "response");
+    debug!(?duration, "duration");
+
+    Ok(())
+}
